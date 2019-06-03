151ec2b8065b5abb98db5887295b67f386278a08
blabadi <bashar.allabadi@oicr.on.ca>
2019-06-03 15:30:25 -0400

    fix kafka song listener test

--- maestro-app/src/test/java/bio/overture/maestro/app/infra/adapter/inbound/messaging/song/SongAnalysisStreamListenerTest.java
+++ maestro-app/src/test/java/bio/overture/maestro/app/infra/adapter/inbound/messaging/song/SongAnalysisStreamListenerTest.java
@@ -21,62 +21,45 @@ import bio.overture.maestro.app.infra.adapter.inbound.messaging.MessagingConfig;
 import bio.overture.maestro.domain.api.Indexer;
 import bio.overture.maestro.domain.api.message.AnalysisIdentifier;
 import bio.overture.maestro.domain.api.message.IndexAnalysisCommand;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.junit.Ignore;
-import org.junit.jupiter.api.Disabled;
+import bio.overture.maestro.domain.api.message.IndexResult;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.messaging.support.GenericMessage;
+import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
-import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
-import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
-
-@Disabled
-@Ignore
-@SpringBootTest(
-    properties = {
-        "embedded.kafka.enabled=true",
-        "embedded.elasticsearch.enabled=false",
-        "embedded.zookeeper.enabled=true",
-        "debug=true"
-    }
-)
-@ContextConfiguration(classes = {SongAnalysisStreamListenerTest.Config.class})
+/*
+ * This test is based on : https://docs.spring.io/spring-cloud-stream/docs/current/reference/htmlsingle/#_testing
+ * Using a real kafka test container didn't turn out as I expected because spring didn't register
+ * my consumers (i.e. didn't do the actual binding and connection with the message source).
+ * However this test does serve the purpose of testing the listener
+ */
+@SpringBootTest(classes = {SongAnalysisStreamListenerTest.MockApplication.class})
 class SongAnalysisStreamListenerTest {
 
-    @Autowired
+    @MockBean
     private Indexer indexer;
 
-    @Value("${embedded.kafka.brokerList}")
-    private String kafkaBrokerList;
+    @Autowired
+    SongAnalysisSink sink;
 
-    @Value("${spring.cloud.stream.bindings.song-input.destination}")
-    private String songAnalysisTopic;
-
-    private String analysisPublishedMessage = "{\n" +
-        "\t\"records\": [\n" +
-        "\t\t{\"value\" : { \"analysisId\" : \"EGAZ00001254368\", \"studyId\" : \"PEME-CA\", \"songServerId\": \"collab\", \"state\": \"PUBLISHED\" }\t}\n" +
-        "\t]\n" +
-    "}";
+    private String analysisPublishedMessage = "{ \"analysisId\" : \"EGAZ00001254368\", \"studyId\" : \"PEME-CA\", " +
+        "\"songServerId\": \"collab\", \"state\": \"PUBLISHED\" }";
 
     @Test
     void shouldIndexOnAnalysisPublishedMessage() throws Exception {
-        sendMessage(songAnalysisTopic, analysisPublishedMessage);
-        Thread.sleep(10000);
+        when(indexer.indexAnalysis(any())).thenReturn(Mono.just(IndexResult.builder().successful(true).build()));
+        sink.songInput().send(new GenericMessage<>(analysisPublishedMessage));
+        Thread.sleep(2000);
         then(indexer).should(times(1)).indexAnalysis(eq(IndexAnalysisCommand.builder()
             .analysisIdentifier(AnalysisIdentifier.builder()
                 .studyId("PEME-CA")
@@ -88,33 +71,12 @@ class SongAnalysisStreamListenerTest {
         );
     }
 
-    private Map<String, Object> getKafkaProducerConfiguration() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(BOOTSTRAP_SERVERS_CONFIG, kafkaBrokerList);
-        configs.put(KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        configs.put(VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        return configs;
-    }
-
-    private void sendMessage(String topic, String message) throws Exception {
-        Map<String, Object> producerConfiguration = getKafkaProducerConfiguration();
-        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(producerConfiguration);
-        kafkaProducer.send(new ProducerRecord<>(topic, message)).get();
-        kafkaProducer.close();
-
-    }
-
-    @Import({
-        MessagingConfig.class
-    })
-    @Configuration
-    static class Config {
-
-        @Bean
-        Indexer mockIndexer() {
-            return mock(Indexer.class);
-        }
-
-    }
+    /*
+     * This is needed or you will get :
+     * org.springframework.integration.MessageDispatchingException: Dispatcher has no subscribers
+     */
+    @SpringBootApplication
+    @Import({MessagingConfig.class})
+    static class MockApplication { }
 }
