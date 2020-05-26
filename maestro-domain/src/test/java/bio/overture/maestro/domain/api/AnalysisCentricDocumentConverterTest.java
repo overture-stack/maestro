3e1016dd25c5ba71c9046a1e5d4010e640fe52e7
Alexis Li <alexis.li@oicr.on.ca>
2020-05-26 16:54:37 -0400

    change specimen type and sample type to list

--- maestro-domain/src/test/java/bio/overture/maestro/domain/api/AnalysisCentricDocumentConverterTest.java
+++ maestro-domain/src/test/java/bio/overture/maestro/domain/api/AnalysisCentricDocumentConverterTest.java
@@ -1,5 +1,6 @@
 package bio.overture.maestro.domain.api;
 
+import bio.overture.maestro.domain.entities.indexing.Sample;
 import bio.overture.maestro.domain.entities.indexing.Specimen;
 import bio.overture.maestro.domain.entities.indexing.analysis.AnalysisCentricDonor;
 import bio.overture.maestro.domain.entities.metadata.study.*;
@@ -20,21 +21,44 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AnalysisCentricDocumentConverterTest {
 
   @Test
-  void testGetDonors(){
+  void testGetDonor() {
+    val analysisObj = loadJsonFixture(this.getClass(), "TEST-CA.analysis.json", Analysis.class);
+
+    // expected:
+    val donor = AnalysisCentricDonor.builder()
+        .id("DO1")
+        .gender("Female")
+        .submitterDonorId("MDT-AP-0749")
+        .specimens(buildSpecimenListForDonor())
+        .build();
+
+    val results = AnalysisCentricDocumentConverter.getDonors(analysisObj);
+
+    assertEquals(1, results.size());
+    assertEquals(donor, results.get(0));
+  }
+
+  @Test
+  void testGetDonors_multi_donor(){
+    // Expected AnalysisCentricDonor data structure:
+    // Analysis  => d1 -> sp1 -> [sa1]
+    //              d1 -> sp2 -> [sa2]
+    //              d2 -> sp3 -> [sa3, sa4]
+    //              d2 -> sp4 -> [sa5, sa6]
     val analysisObj = loadJsonFixture(this.getClass(),
             "TEST-CA.analysis.multi-donor.json", Analysis.class);
 
-    // expected result:
+    // expected results:
     val donor_1 = AnalysisCentricDonor.builder()
             .id("DO1")
-            .gender("female")
+            .gender("Female")
             .submitterDonorId("MDT-AP-0749")
             .specimens(buildSpecimenListForDonor1())
             .build();
 
     val donor_2 = AnalysisCentricDonor.builder()
             .id("DO2")
-            .gender("female")
+            .gender("Female")
             .submitterDonorId("MDT-AP-0749")
             .specimens(buildSpecimenListForDonor2())
             .build();
@@ -42,63 +66,119 @@ public class AnalysisCentricDocumentConverterTest {
     val results = AnalysisCentricDocumentConverter.getDonors(analysisObj);
 
     assertNotNull(results);
-    assertEquals(results.size(), 2);
-    assertTrue(results.contains(donor_1));
-    assertTrue(results.contains(donor_2));
+    assertEquals(2, results.size());
+    assertEquals(donor_2, results.get(0));
+    assertEquals(donor_1, results.get(1));
   }
 
-  private List<bio.overture.maestro.domain.entities.indexing.Specimen> buildSpecimenListForDonor1() {
-    val specimen_1 = bio.overture.maestro.domain.entities.indexing.Specimen.builder()
+  private List<Specimen> buildSpecimenListForDonor() {
+    val specimen = Specimen.builder()
+        .id("SP1")
+        .specimenTissueSource("Other")
+        .submitterSpecimenId("MDT-AP-0749_tumor_specimen")
+        .tumourNormalDesignation("Tumour")
+        .specimenType("Primary tumour - solid tissue")
+        .samples(List.of(
+            Sample.builder()
+                .id("SA1")
+                .sampleType("DNA")
+                .submitterSampleId("MDT-AP-0749_tumor")
+                .matchedNormalSubmitterSampleId("PCSI_0216_St_R")
+                .build()))
+        .build();
+    return List.of(specimen);
+  }
+
+  private List<Specimen> buildSpecimenListForDonor1() {
+    val specimen_1 = Specimen.builder()
             .id("SP1")
             .specimenTissueSource("Other")
             .submitterSpecimenId("MDT-AP-0749_tumor_specimen")
             .tumourNormalDesignation("Tumour")
             .specimenType("Primary tumour - solid tissue")
-            .samples(bio.overture.maestro.domain.entities.indexing.Sample.builder()
+            .samples(List.of(
+                  Sample.builder()
                     .id("SA1")
                     .sampleType("DNA")
                     .submitterSampleId("MDT-AP-0749_tumor")
                     .matchedNormalSubmitterSampleId("PCSI_0216_St_R")
-                    .build())
+                    .build()))
             .build();
 
-    val specimen_2 = bio.overture.maestro.domain.entities.indexing.Specimen.builder()
+    val specimen_2 = Specimen.builder()
             .id("SP2")
             .specimenTissueSource("Other")
             .submitterSpecimenId("MDT-AP-0749_tumor_specimen")
             .tumourNormalDesignation("Tumour")
             .specimenType("Primary tumour - solid tissue")
-            .samples(bio.overture.maestro.domain.entities.indexing.Sample.builder()
+            .samples(List.of(
+                Sample.builder()
                     .id("SA2")
                     .sampleType("DNA")
                     .submitterSampleId("MDT-AP-0749_tumor")
                     .matchedNormalSubmitterSampleId("PCSI_0216_St_R")
-                    .build())
+                    .build()))
             .build();
 
     val list = new ArrayList<Specimen>();
-    list.add(specimen_1);
     list.add(specimen_2);
+    list.add(specimen_1);
     return list;
   }
 
-  private List<bio.overture.maestro.domain.entities.indexing.Specimen> buildSpecimenListForDonor2() {
-    val specimen_1 = bio.overture.maestro.domain.entities.indexing.Specimen.builder()
+  private List<Specimen> buildSpecimenListForDonor2() {
+    val specimen_3 = Specimen.builder()
             .id("SP3")
             .specimenTissueSource("Other")
             .submitterSpecimenId("MDT-AP-0749_tumor_specimen")
             .tumourNormalDesignation("Tumour")
             .specimenType("Primary tumour - solid tissue")
-            .samples(bio.overture.maestro.domain.entities.indexing.Sample.builder()
-                    .id("SA3")
-                    .sampleType("DNA")
-                    .submitterSampleId("MDT-AP-0749_tumor")
-                    .matchedNormalSubmitterSampleId("PCSI_0216_St_R")
-                    .build())
+            .samples(buuldSamplesForDonor2_sp3())
+            .build();
+    val specimen_4 = Specimen.builder()
+            .id("SP4")
+            .specimenTissueSource("Other")
+            .submitterSpecimenId("MDT-AP-0749_tumor_specimen")
+            .tumourNormalDesignation("Tumour")
+            .specimenType("Primary tumour - solid tissue")
+            .samples(buildSamplesForDonor2_sp4())
             .build();
 
     val list = new ArrayList<Specimen>();
-    list.add(specimen_1);
+    list.add(specimen_4);
+    list.add(specimen_3);
     return list;
+  }
+
+  private List<Sample> buuldSamplesForDonor2_sp3() {
+    val sample_3 = Sample.builder()
+        .id("SA3")
+        .sampleType("DNA")
+        .submitterSampleId("MDT-AP-0749_tumor")
+        .matchedNormalSubmitterSampleId("PCSI_0216_St_R")
+        .build();
+    val sample_4 = Sample.builder()
+        .id("SA4")
+        .sampleType("DNA")
+        .submitterSampleId("MDT-AP-0749_tumor")
+        .matchedNormalSubmitterSampleId("PCSI_0216_St_R")
+        .build();
+    return List.of(sample_3, sample_4);
+  }
+
+  private List<Sample> buildSamplesForDonor2_sp4() {
+    val sample_5 = Sample.builder()
+        .id("SA5")
+        .sampleType("DNA")
+        .submitterSampleId("MDT-AP-0749_tumor")
+        .matchedNormalSubmitterSampleId("PCSI_0216_St_R")
+        .build();
+    val sample_6 = Sample.builder()
+        .id("SA6")
+        .sampleType("DNA")
+        .submitterSampleId("MDT-AP-0749_tumor")
+        .matchedNormalSubmitterSampleId("PCSI_0216_St_R")
+        .build();
+    return List.of(sample_5, sample_6);
   }
 }
