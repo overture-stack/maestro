9cba32900793a21d7f0b0da6001f024ae4d13cd2
Leonardo Rivera <leorivera_88@hotmail.com>
2025-01-08 10:53:35 -0500

    adding more indexer unit tests

--- packages/indexer-client/test/createClient.spec.ts
+++ packages/indexer-client/test/createClient.spec.ts
@@ -2,10 +2,42 @@ import { expect } from 'chai';
 
 import { es7 } from '../src/client/v7/client.js';
 import { es8 } from '../src/client/v8/client.js';
+import { clientProvider } from '../src/index.js';
 
-describe('Initialize Client', () => {
-	describe('Client V7', () => {
-		it('should provide indexing functions', () => {
+describe('Initialize Indexer', () => {
+	describe('Provider', () => {
+		it('should create a provider V7 with indexing functions', () => {
+			const nodeUrl = 'http://myserver:9200';
+			const providerConfig = { nodes: nodeUrl, basicAuth: { enabled: false }, version: 7 };
+			const provider = clientProvider(providerConfig);
+			expect(provider).to.have.property('createIndex');
+			expect(provider).to.have.property('addData');
+			expect(provider).to.have.property('updateData');
+			expect(provider).to.have.property('deleteData');
+			expect(provider).to.have.property('ping');
+		});
+		it('should create a provider V8 with indexing functions', () => {
+			const nodeUrl = 'http://myserver:9200';
+			const providerConfig = { nodes: nodeUrl, basicAuth: { enabled: false }, version: 8 };
+			const provider = clientProvider(providerConfig);
+			expect(provider).to.have.property('createIndex');
+			expect(provider).to.have.property('addData');
+			expect(provider).to.have.property('updateData');
+			expect(provider).to.have.property('deleteData');
+			expect(provider).to.have.property('ping');
+		});
+		it('should throw an error if configuration version is incorrect', () => {
+			try {
+				const nodeUrl = 'http://myserver:9200';
+				const providerConfig = { nodes: nodeUrl, basicAuth: { enabled: false }, version: 10 };
+				clientProvider(providerConfig);
+			} catch (error) {
+				expect(error.message).to.eql('Unsupported Elasticsearch version');
+			}
+		});
+	});
+	describe('Client', () => {
+		it('should create a client V7 with indexing functions', () => {
 			const nodeUrl = 'http://myserver:9200';
 			const client = es7({ nodes: nodeUrl, basicAuth: { enabled: false }, version: 7 });
 
@@ -15,9 +47,7 @@ describe('Initialize Client', () => {
 			expect(client).to.have.property('deleteData');
 			expect(client).to.have.property('ping');
 		});
-	});
-	describe('Client V8', () => {
-		it('should provide indexing functions', () => {
+		it('should create a client V8 with indexing functions', () => {
 			const nodeUrl = 'http://myserver:9200';
 			const client = es8({ nodes: nodeUrl, basicAuth: { enabled: false }, version: 8 });
 
@@ -26,6 +56,24 @@ describe('Initialize Client', () => {
 			expect(client).to.have.property('updateData');
 			expect(client).to.have.property('deleteData');
 			expect(client).to.have.property('ping');
+		});
+		it('should throw an error if client V7 configuration version is incorrect', () => {
+			const nodeUrl = 'http://myserver:9200';
+
+			try {
+				es7({ nodes: nodeUrl, basicAuth: { enabled: false }, version: 10 });
+			} catch (error) {
+				expect(error.message).to.eql('Invalid Client Configuration');
+			}
+		});
+		it('should throw an error if client V8 configuration version is incorrect', () => {
+			const nodeUrl = 'http://myserver:9200';
+
+			try {
+				es8({ nodes: nodeUrl, basicAuth: { enabled: false }, version: 10 });
+			} catch (error) {
+				expect(error.message).to.eql('Invalid Client Configuration');
+			}
 		});
 	});
 });
